d96b8940a80355bce44126a6e7ed5242f860ba80
GerwenRick <rickvvgerwen@live.nl>
2025-04-11 20:25:55 +0200

    page styling added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,13 @@ export const routes: Routes = [
       import('./homepage/homepage.component').then((m) => m.HomepageComponent),
   },
   {
+    path: 'plattegrond',
+    loadComponent: () =>
+      import('./plattegrond/plattegrond.component').then(
+        (m) => m.PlattegrondComponent
+      ),
+  },
+  {
     path: 'config',
     loadComponent: () =>
       import('./mqtt/mqtt.component').then((m) => m.MqttComponent),
